7a1c261ce010df59a0c34763ebc5512aebb71bd1
Jing Conan Wang <wangjing@bu.edu>
2012-08-15 21:31:52 -0400

    GUI add startgui.sh loadguiconfig.sh

--- gui/run.sh
+++ gui/run.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
 export LIBDIR=.
-wish core.tcl ./sample.imm
+wish core.tcl ./sample.imn
 # wish core.tcl
 # wish core.tcl ./configs/sample3-bgp.imn
--- loadguiconfig.sh
+++ loadguiconfig.sh
@@ -1,2 +1,5 @@
 #!/usr/bin/env sh
-./sim -e ManualTopoExperiment --mode sim -s file_exfiltration -f gui/imalse_config/topology.inet --net_settings=gui/imalse_config/net_settings.py -t 100
+./sim -e ManualTopoExperiment --mode sim -s file_exfiltration -f \
+    gui/imalse_config/topology.inet \
+    --net_settings=gui/imalse_config/net_settings.py \
+    -t 100 --SimulatorImpl=Visual
